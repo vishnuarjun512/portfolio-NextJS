8593046e6eda39bf198b17e79348295b92cfc977
vishnuarjun512 <vishnuarjun512@gmail.com>
2023-12-28 19:38:36 +0530

    Added New Projects Data

--- components/contents/ProjectItem.jsx
+++ components/contents/ProjectItem.jsx
@@ -3,33 +3,47 @@ import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
 import { MdOutlineWeb } from "react-icons/md";
 
-const ProjectItem = ({ image, name, tech, hosted }) => {
+const ProjectItem = ({ image, name, tech, hosted, link }) => {
   return (
-    <div className="min-h-[200px] md:min-h-[350px] md:max-h-[500px] max-w-[100%] relative flex items-center justify-center group h-full w-full  shadow-gray-400 rounded-xl hover:bg-gradient-to-b hover:scale-105 from-gray-700 to-[#14131a] ease-in-out duration-700">
+    <div className="min-h-[200px] md:min-h-[350px] md:max-h-[500px] max-w-[100%] relative flex items-center justify-center group h-full w-full shadow-gray-400 rounded-xl hover:bg-gradient-to-b hover:scale-105 from-gray-700 to-[#14131a] ease-in-out duration-700">
       <Image
         className="w-full h-full rounded-xl group-hover:opacity-[0.1] object-cover transition-all duration-700 "
         src={image}
         alt="/"
       />
-      <div className="absolute group-hover:flex hidden flex-col gap-3 justify-center items-center text-white">
-        <div className="text-[30px] sm:text-[50px] font-bold">{name}</div>
-        <div className="flex text-[12px] sm:text-[20px]">
-          {tech.map((t, index) => (
-            <React.Fragment key={index}>
-              {t}
-              {index !== tech.length - 1 && ", "}
-            </React.Fragment>
-          ))}
-        </div>
-        <div className="flex gap-2">
-          <a className="flex gap-1 hover:scale-110 duration-200 transition-all ease-in-out items-center justify-center bg-black hover:bg-white cursor-pointer hover:text-black font-semibold text-white p-2 rounded-lg">
-            <MdOutlineWeb className="w-8 h-8" />
-            <p>Hosted</p>
-          </a>
-          <a className="flex gap-1 hover:scale-110 duration-200 transition-all ease-in-out items-center justify-center bg-black hover:bg-white cursor-pointer hover:text-black font-semibold text-white p-2 rounded-lg">
-            <FaGithub className="w-8 h-8" />
-            <p>Code</p>
-          </a>
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] hover:scale-105 transition-all duration-300 text-white z-10">
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-[20px] sm:text-[40px] whitespace-nowrap font-bold text-center">
+            {name}
+          </p>
+          <div className="flex text-[12px] sm:text-[20px] text-center md:whitespace-nowrap">
+            {tech.map((t, index) => (
+              <React.Fragment key={index}>
+                {t}
+                {index !== tech.length - 1 && ", "}
+              </React.Fragment>
+            ))}
+          </div>
+          <div className="flex gap-2">
+            {hosted.yes && (
+              <a
+                href={hosted.link}
+                target="_blank"
+                className="flex gap-1 md:gap-2 hover:scale-110 duration-200 transition-all ease-in-out items-center justify-between bg-black hover:bg-white cursor-pointer hover:text-black font-semibold text-white p-2 md:px-5 rounded-lg"
+              >
+                <MdOutlineWeb className="w-8 h-8" />
+                <p>Hosted</p>
+              </a>
+            )}
+            <a
+              href={link}
+              target="_blank"
+              className="flex gap-1 md:gap-2 hover:scale-110 duration-200 transition-all ease-in-out items-center justify-between bg-black hover:bg-white cursor-pointer hover:text-black font-semibold text-white p-2 md:px-5 rounded-lg"
+            >
+              <FaGithub className="w-8 h-8" />
+              <p>Code</p>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- components/contents/Projects.jsx
+++ components/contents/Projects.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Rozilla from "@/public/rozilla.png";
 import Portfolio from "@/public/portfolio.png";
+import Portfolio2 from "@/public/portfolio2.png";
+import MTVX from "@/public/mtvx.png";
+import imageGallery from "@/public/imageGallery.png";
+import Calculator from "@/public/calculator.png";
+import Todo from "@/public/todo.png";
+import DA from "@/public/dataAnalytics.png";
 import Planti from "@/public/planti.png";
 import ProjectItem from "./ProjectItem";
 
-const projects = [
+const projects2 = [
   {
     name: "Rozilla",
     tech: ["MERN Stack", "Tailwind", "Firebase", "OAuth"],
@@ -26,18 +32,104 @@ const projects = [
     image: Portfolio,
     category: "Frontend",
   },
+];
+
+const projects = [
+  {
+    name: "Rozilla RealEstate",
+    link: "https://github.com/vishnuarjun512/mern-RealEstate",
+    tech: ["MERN Stack", "TailwindCSS", "Redux", "Firebase"],
+    image: Rozilla,
+    hosted: {
+      yes: true,
+      link: "https://rozilla-realestate.onrender.com/",
+    },
+    category: "FullStack",
+  },
+
+  {
+    name: "Image Gallery",
+    link: "https://github.com/vishnuarjun512/unsplash-image-gallery-ReactJS",
+    tech: ["ReactJS", "TailwindCSS", "Unsplash API"],
+    image: imageGallery,
+    hosted: {
+      yes: false,
+      link: "",
+    },
+    category: "Frontend",
+  },
   {
     name: "Todo Web API",
     tech: ["ExpressJS", "Postman", "Swagger"],
-    hosted: "",
-    image: Portfolio,
+    link: "https://github.com/vishnuarjun512/Task-Manager-API",
+    image: Todo,
+    hosted: {
+      yes: true,
+      link: "https://documenter.getpostman.com/view/30059269/2s9YeD9DRt",
+    },
     category: "Backend",
+  },
+  {
+    name: "MTVx Movies",
+    link: "https://github.com/vishnuarjun512/Movie-Website/tree/dev",
+    tech: ["ReactJS", "TailwindCSS", "MovieListAPI"],
+    image: MTVX,
+    hosted: {
+      yes: false,
+      link: "",
+    },
+    category: "Frontend",
+  },
+  {
+    name: "Plantii Ecommerce",
+    link: "https://github.com/vishnuarjun512/Plant_Ecommerce",
+    tech: ["ReactJS", "SASS"],
+    image: Planti,
+    hosted: {
+      yes: false,
+      link: "",
+    },
+    category: "Frontend",
+  },
+  {
+    name: "Data Analytics",
+    link: "https://github.com/vishnuarjun512/Data-Finance-React",
+    tech: ["ReactJS", "Tailwind"],
+    image: DA,
+    hosted: {
+      yes: false,
+      link: "",
+    },
+    category: "Frontend",
+  },
+  {
+    name: "Portfolio",
+    link: "https://github.com/vishnuarjun512/portfolio",
+    tech: ["ReactJS", "TailwindCSS"],
+    image: Portfolio,
+    hosted: {
+      yes: true,
+      link: "https://vishnuarjun512.github.io/portfolio/",
+    },
+    category: "Frontend",
+  },
+  {
+    name: "Simple Calculator",
+    link: "https://github.com/vishnuarjun512/Calculator",
+    tech: ["Javascript"],
+    image: Calculator,
+    hosted: {
+      yes: true,
+      link: "https://vishnuarjun512.github.io/Calculator/",
+    },
+    category: "Frontend",
   },
   {
     name: "Portfolio NextJS",
     tech: ["NextJS"],
+    link: "https://github.com/vishnuarjun512/portfolio-NextJS",
     hosted: "",
-    image: Portfolio,
+    image: Portfolio2,
     category: "NextJS",
   },
 ];
@@ -80,7 +172,7 @@ const Projects = () => {
                   category == uniqueCategories[index]
                     ? "bg-purple-600 hover:bg-purple-500 border-[2px] border-white text-white"
                     : "hover:bg-green-200"
-                } w-full md:w-fit md:px-20 p-2 py-3 md:py-4 text-[12px] lg:text-2xl md:text-xl sm:text-base text-center whitespace-nowrap font-semibold transition-all duration-200 ease-in-out bg-green-50 rounded-lg`}
+                } w-full md:w-fit md:px-6 lg:px-20 p-2 py-3 md:py-4 text-[12px] lg:text-2xl md:text-xl sm:text-base text-center whitespace-nowrap font-semibold transition-all duration-200 ease-in-out bg-green-50 rounded-lg`}
                 key={index}
                 onClick={() => {
                   setCategory(uniqueCategories[index]);
@@ -91,7 +183,7 @@ const Projects = () => {
             );
           })}
         </div>
-        <div className="bg-blue-400 grid md:grid-cols-2 place-items-center content-start gap-5 p-4 max-h-[480px] md:max-h-[680px] min-h-[220px] md:min-h-[410px] overflow-auto">
+        <div className="bg-blue-400 rounded-lg grid md:grid-cols-2 place-items-center content-start gap-5 p-5 max-h-[480px] md:max-h-[680px] min-h-[220px] md:min-h-[410px] overflow-auto">
           {displayingProjects?.map((p, index) => {
             return (
               <ProjectItem
